c4d2a311a49e1a60c70c9c8aaa5425785315e78d
Andrew Giugliano <agiuglia@umich.edu>
2017-10-28 16:10:40 -0400

    Initial Commit

---
+++ app/src/main/java/com/dev/andrewgiugliano/daggercommitwatcher/CommitWatchList.java
@@ -0,0 +1,13 @@
+package com.dev.andrewgiugliano.daggercommitwatcher;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CommitWatchList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_commit_watch_list);
+    }
+}
